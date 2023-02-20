d44c69f5b3568e7768509c4b1db5b5255acc6491
Lesleis Nagy <lesleisnagy@gmail.com>
2023-02-20 11:41:05 +0000

    Removed requirement for Cerberus.
    Cerberus is removed and we now use 'schematics' for serialization/deserialization and validation.
    The 'read_config_from_environ' function now cached the configuration object.

--- lib/m4db_database/configuration.py
+++ lib/m4db_database/configuration.py
@@ -2,126 +2,91 @@ import os
 
 import yaml
 
-from cerberus import Validator
+from schematics.models import Model
+from schematics.types import StringType
+from schematics.types.compound import ModelType
 
 from m4db_database.decorators import static
 
 from m4db_database import GLOBAL
 
 
-class Database:
+class Database(Model):
     r"""
-    Class to hold database information.
+    Class to hold configuration information about databases for M4DB_DATABASE.
     """
-    def __init__(self):
-        self.type = None
-        self.uri = None
-        self.file_root = None
+    type = StringType(required=True, regex=r"POSTGRES")
+    uri = StringType(required=True)
+    file_root = StringType(required=True, serialized_name="file-root")
 
 
-class Configuration:
+class Configuration(Model):
     r"""
     Class to hold configuration information for M4DB_DATABASE.
     """
-    def __init__(self):
-        self.password_salt = None
-        self.database = None
+    password_salt = StringType(required=True, serialized_name="password-salt")
+    database = ModelType(Database)
 
 
-@static(validator=None)
-def validation_schema() -> Validator:
-    r"""
-    Generate a cerberus validations schema to check configuration files.
-    :return a cerberus validation schema"
-    """
-    if validation_schema.validator is None:
-        validation_schema.validator = Validator()
-
-        validation_schema.validator.schema = {
-            "password-salt": {"type": "string", "required": True},
-            "database": {
-                "type": "dict",
-                "schema": {
-                    "type": {"type": "string", "regex": r"POSTGRES", "required": True},
-                    "uri": {"type": "string", "required": True},
-                    "file-root": {"type": "string", "required": True}
-                },
-                "required": True
-            }
-        }
-
-    return validation_schema.validator
-
-
-@static(config=None, file_name=None)
-def read_config_from_file(file_name):
+def read_config_from_file(file_name: str) -> Configuration:
     r"""
     Reads an M4DB database configuration from a file, the file format must match the validation schema above.
+
     :param file_name the file name containing configuration information.
 
+    :return: a configuration object.
     """
+    # If the config cache is empty or the file_name associated with a config has changed, then cache a new config.
+    with open(file_name, "r") as fin:
+        config_dict = yaml.load(fin, Loader=yaml.FullLoader)
+        config = Configuration(config_dict)
 
-    if read_config_from_file.config is None or read_config_from_file.file_name != file_name:
-        # If the config cache is empty or the file_name associated with a config has changed, then cache a new config.
-        with open(file_name, "r") as fin:
-            config_dict = yaml.load(fin, Loader=yaml.FullLoader)
-            if validation_schema().validate(config_dict):
-                config = Configuration()
-                config.database = Database()
-                config.database.type = config_dict["database"]["type"]
-                config.database.uri = config_dict["database"]["uri"]
-                config.database.file_root = config_dict["database"]["file-root"]
-                read_config_from_file.config = config
-                read_config_from_file.file_name = file_name
-            else:
-                raise ValueError(f"Configuration file '{file_name}' is not valid.")
-
-    return read_config_from_file.config
+    return config
 
 
-def read_config_from_environ():
+@static(config=None)
+def read_config_from_environ(force_reload: bool = False) -> Configuration:
     r"""
     Reads an M4DB database configuration by checking for an environment variable called 'M4DB_CONFIG".
 
-    Returns:
-        A python dictionary representation of M4DB database related configuration information.
+    :param force_reload: ignore cached config info and just force the configuration information to be reloaded.
 
+    :return: A python dictionary representation of M4DB database related configuration information.
     """
-    file_name = os.environ.get(GLOBAL.M4DB_DATABASE_CONFIG_ENV_VAR)
+    self = read_config_from_environ
+    if self.config is None or force_reload is True:
+        file_name = os.environ.get(GLOBAL.M4DB_DATABASE_CONFIG_ENV_VAR)
+        if file_name is None:
+            raise ValueError(f"{GLOBAL.M4DB_DATABASE_CONFIG_ENV_VAR} environment variable doesn't exist")
+        self.config = read_config_from_file(file_name)
 
-    if file_name is None:
-        raise ValueError(f"{GLOBAL.M4DB_DATABASE_CONFIG_ENV_VAR} environment variable doesn't exist")
-
-    return read_config_from_file(file_name)
+    return self.config
 
 
-def write_config_to_file(file_name, config):
+def write_config_to_file(file_name: str, config: Configuration):
     r"""
     Writes M4DB database configuration.
 
-    file_name: the file name to write configuration data to.
-    config: A python dictionary representation of M4DB database related configuration information.
+    :param file_name: the file name to write configuration data to.
+    :param config: a configuration object.
 
-    Returns:
-        None.
-
+    :return: None.
     """
     with open(file_name, "w") as fout:
-        yaml.dump(config, fout, default_flow_style=False)
+        yaml.dump(config.to_primitive(), fout, default_flow_style=False)
 
 
-def write_config_to_environ(config):
+def write_config_to_environ(config: Configuration):
     r"""
     Writes an M4DB database configuration by checking for an environment variable called 'M4DB_CONFIG".
-    Args:
-        config: the configuration data that is to be written to the environment config.
-    Returns:
-        None
 
+    :param config: the configuration data that is to be written to the environment config.
+
+    :return: None.
     """
     file_name = os.environ.get(GLOBAL.M4DB_DATABASE_CONFIG_ENV_VAR)
 
     if file_name is None:
         raise ValueError(f"{GLOBAL.M4DB_DATABASE_CONFIG_ENV_VAR} environment variable doesn't exist")
 
-    return write_config_to_file(file_name, config)
+    return write_config_to_file(file_name, config.to_primitive())
