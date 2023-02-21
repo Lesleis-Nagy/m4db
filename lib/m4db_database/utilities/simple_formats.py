47d04e0a6e17ceb9b77d260ad5ad7c9d6dfee347
Lesleis Nagy <lesleisnagy@gmail.com>
2023-02-21 11:01:25 +0000

    Added routines to check for string formats against regular expressions.

---
+++ lib/m4db_database/utilities/simple_formats.py
@@ -0,0 +1,15 @@
+r"""
+A selection of utility functions to check for simple data format consistency.
+"""
+
+import re
+
+from m4db_database.decorators import static
+
+@static(regex_decimal=re.compile(r"([0-9]+)(\.(([0-9])+)?)?"))
+def is_str_decimal(str_decimal: str) -> bool:
+    self = is_str_decimal
+    if self.regex_decimal.match(str_decimal):
+        return True
+    else:
+        return False
