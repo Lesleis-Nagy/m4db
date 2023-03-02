62a7b012db7a795d37e28825c0eb119352483b6a
Lesleis Nagy <lesleisnagy@gmail.com>
2023-03-02 11:31:42 +0000

    Added empty software to database for test.

--- test/lib/m4db_database_test/setup_dataset_1.py
+++ test/lib/m4db_database_test/setup_dataset_1.py
@@ -21,42 +21,47 @@ def setup_dataset_1():
     shutil.rmtree(file_root, ignore_errors=True)
 
     # Drop database.
-    proc1 = Popen(
+    proc = Popen(
         f"echo 'drop database {db_name}' | psql -U postgres -h localhost",
         shell=True, universal_newlines=True
     )
-    stdout, stderr = proc1.communicate()
+    stdout, stderr = proc.communicate()
 
     # Create database.
-    proc2 = Popen(
+    proc = Popen(
         f"echo 'create database {db_name}' | psql -U postgres -h localhost",
         shell=True, universal_newlines=True)
-    stdout, stderr = proc2.communicate()
+    stdout, stderr = proc.communicate()
 
     # Regenerate database.
-    proc3 = Popen(
+    proc = Popen(
         f"m4db-setup-database {db_name} {file_root} --yes-to-all",
         shell=True, universal_newlines=True)
-    stdout, stderr = proc3.communicate()
+    stdout, stderr = proc.communicate()
 
     # Populate with required test data.
 
-    proc4 = Popen(
+    proc = Popen(
         "m4db-user add testuser Test User test.user@testurl.com",
         shell=True, universal_newlines=True)
-    stdout, stderr = proc4.communicate()
+    stdout, stderr = proc.communicate()
 
-    proc5 = Popen(
+    proc = Popen(
         "m4db-project add testproject 'A test project'",
         shell=True, universal_newlines=True)
-    stdout, stderr = proc5.communicate()
+    stdout, stderr = proc.communicate()
 
-    proc6 = Popen(
+    proc = Popen(
         "m4db-software add merrill 1.8.1 --executable='/home/m4dbdev/Install/merrill/1.8.1/bin/merrill'",
         shell=True, universal_newlines=True)
-    stdout, stderr = proc6.communicate()
+    stdout, stderr = proc.communicate()
 
-    proc7 = Popen(
+    proc = Popen(
+        "m4db-software add emptysw 0.1.0",
+        shell=True, universal_newlines=True)
+    stdout, stderr = proc.communicate()
+
+    proc = Popen(
         "m4db-geometry add-ellipsoid "
         "/data/meshes/all-ellipsoids/ellipsoid_40nm_3nm_pro_1p400_obl_1p700.pat "
         "0.040 0.003 ESVD 1.4 1.7 "
@@ -65,9 +70,9 @@ def setup_dataset_1():
         "--mesh-gen-stdout /data/meshes/all-ellipsoids/ellipsoid_40nm_3nm_pro_1p400_obl_1p700.stdout "
         "--unique-id 04c5e362-3c21-485b-8983-bbde335d60fc",
         shell=True, universal_newlines=True)
-    stdout, stderr = proc7.communicate()
+    stdout, stderr = proc.communicate()
 
-    proc8 = Popen(
+    proc = Popen(
         "m4db-geometry add-ellipsoid /data/meshes/all-ellipsoids/ellipsoid_40nm_3nm_pro_1p400_obl_1p000.pat "
         "0.040 0.003 ESVD 1.4 1.0 "
         "--exodus-file /data/meshes/all-ellipsoids/ellipsoid_40nm_3nm_pro_1p400_obl_1p000.e "
@@ -75,10 +80,10 @@ def setup_dataset_1():
         "--mesh-gen-stdout /data/meshes/all-ellipsoids/ellipsoid_40nm_3nm_pro_1p400_obl_1p000.stdout "
         "--unique-id 77fa4246-2378-4fc1-99de-92bd4479202f",
         shell=True, universal_newlines=True)
-    stdout, stderr = proc8.communicate()
+    stdout, stderr = proc.communicate()
 
-    proc9 = Popen(
+    proc = Popen(
         "m4db-model add /data/datasets/new_models_test_specimen_01.json "
         "testuser testproject merrill 1.8.1 --no-dry-run",
         shell=True, universal_newlines=True)
-    stdout, stderr = proc9.communicate()
+    stdout, stderr = proc.communicate()
